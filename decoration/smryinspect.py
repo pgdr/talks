965960941059763af8365e99a23e5ed192048040
Pål Grønås Drange <PGDR@statoil.com>
2017-12-04 16:39:23 +0100

    Add non-interactive inspect mode

--- decoration/smryinspect.py
+++ decoration/smryinspect.py
@@ -2,8 +2,10 @@
 from pandas import DataFrame
 from ecl.ecl import EclSum
 
+
 class memoize(dict):
     """Memoization wrapper for unary function."""
+
     def __init__(self, fun):
         self.fun = fun
 
@@ -13,6 +15,7 @@ class memoize(dict):
     def __missing__(self, args):
         y = self[args] = self.fun(*args)
         return y
+
 
 @memoize
 def edit(s, t):
@@ -51,26 +54,51 @@ def find_closest(kw, keys):
             matches.append(key)
     return best_score, matches
 
-def main(fname):
+
+def load_smry(fname):
+    """Returns (smry,list of keywords)."""
     smry = EclSum(fname)
-    keys = list(smry.keys())
+    return smry, list(smry.keys())
+
+
+def lookup(smry, keys, kw):
+    """Prints kw vector or "did you mean".  """
+    if kw in keys:
+        print(to_pandas(kw, smry))
+    else:
+        score, match = find_closest(kw, keys)
+        if score <= 4:
+            print('Did you mean (score: %d): \n\t%s' % (score,
+                                                        '\n\t'.join(match)))
+        else:
+            print('No such keyword.  Try [L]ist to see keywords.')
+
+
+def interactive(smry, keys):
+    """Interactive smry inspector."""
     kw = None
     while kw != 'q':
         kw = raw_input('Enter kw (q to exit): ').upper().strip()
-        if kw == 'Q':
+        if kw in ('Q', 'QUIT'):
             return
-        if kw == 'L':
+        if kw in ('L', 'LIST'):
             print('\n\t'.join(keys))
             continue
-        if kw in keys:
-            print(to_pandas(kw, smry))
-        else:
-            score, match = find_closest(kw, keys)
-            print('Did you mean (score: %d): \n\t%s' % (score,
-                                                        '\n\t'.join(match)))
+        if kw in ('H', 'HELP'):
+            print('Commands: Q (quit), L (list), H (this)')
+            continue
+        lookup(smry, keys, kw)
+
 
 if __name__ == '__main__':
     from sys import argv
-    if len(argv) != 2:
-        exit('usage: smryinspect.py ~/statoil/norne/ECL.2014.2/NORNE_ATW2013.UNSMRY')
-    main(argv[1])
+    if len(argv) not in (2, 3):
+        exit("""usage: smryinspect.py NORNE_ATW2013.UNSMRY
+       smryinspect.py NORNE_ATW2013.UNSMRY FOPR
+        """)
+
+    smry, keys = load_smry(argv[1])
+    if len(argv) == 2:
+        interactive(smry, keys)
+    else:
+        lookup(smry, keys, argv[2])
