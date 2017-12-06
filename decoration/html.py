645de9581be4124da7db3ddf7e29b56948e2abd7
Pål Grønås Drange <PGDR@statoil.com>
2017-12-06 22:52:08 +0100

    Add html.py example

---
+++ decoration/html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+def html(fun):
+    def wrapper(arg):
+        print('<p>')
+        x = fun(arg)
+        print('</p>')
+        return x
+    return wrapper
+
+
+@html
+def write(text):
+    print(text)
+    return 7
+
+
+x = write('hello worlds')
+print(x)
+
+# <h1>
+# hello worlds
+# </h1>
